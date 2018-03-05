fb7b68920b30ff110d33481095d0e7dfe8d93faf
ksuddards <kmsuddards@gmail.com>
2018-03-05 07:09:55 -0800

    .

--- src/test/java/tests/facebookTest.java
+++ src/test/java/tests/facebookTest.java
@@ -36,38 +36,44 @@ public class facebookTest {
 
         //5. FILL IN THE TEXT BOXES: FIRST NAME, SURNAME, MOBILE NUMBER OR EMAIL ADDRESS, “RE-ENTER MOBILE NUMBER”, NEW PASSWORD.
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"u_0_e\"]")));
-        driver.manage().timeouts().implicitlyWait(2, SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, SECONDS);
         driver.findElement(By.xpath("//*[@id=\"u_0_e\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"u_0_e\"]")).sendKeys("James");
+        driver.findElement(By.xpath("//*[@id=\"u_0_e\"]")).sendKeys("Jane");
         driver.findElement(By.xpath("//*[@id=\"u_0_g\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"u_0_g\"]")).sendKeys("Smith");
+        driver.findElement(By.xpath("//*[@id=\"u_0_g\"]")).sendKeys("Jones");
         driver.findElement(By.xpath("//*[@id=\"u_0_j\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"u_0_j\"]")).sendKeys("james1234@gmail.com");
+        driver.findElement(By.xpath("//*[@id=\"u_0_j\"]")).sendKeys("janes12345@gmail.com");
         driver.findElement(By.xpath("//*[@id=\"u_0_q\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"u_0_q\"]")).sendKeys("jamespassword");
+        driver.findElement(By.xpath("//*[@id=\"u_0_q\"]")).sendKeys("janespassword");
         driver.findElement(By.xpath("//*[@id=\"u_0_b\"]")).click();
         driver.findElement(By.xpath("//*[@id=\"u_0_m\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"u_0_m\"]")).sendKeys("james1234@gmail.com");
+        driver.findElement(By.xpath("//*[@id=\"u_0_m\"]")).sendKeys("janes12345@gmail.com");
 
         //6. UPDATE THE DATE OF BIRTH IN THE DROPDOWN.
 
         Select month = new Select(driver.findElement(By.xpath("//*[@id=\"month\"]")));
-        month.selectByVisibleText("Feb");
+        month.selectByVisibleText("Jan");
+        driver.manage().timeouts().implicitlyWait(2, SECONDS);
         Select day = new Select(driver.findElement(By.xpath("//*[@id=\"day\"]")));
         month.selectByValue("1");
+        driver.manage().timeouts().implicitlyWait(2, SECONDS);
 
         //7. SELECT THE GENDER.
         driver.findElement(By.xpath("//*[@id=\"u_0_b\"]")).click();
+        driver.manage().timeouts().implicitlyWait(10, SECONDS);
 
         //8. CLICK ON “CREATE AN ACCOUNT”.
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"u_0_t\"]")));
-        driver.findElement(By.xpath("//*[@id=\"u_0_t\"]")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"u_0_w\"]")));
+        driver.findElement(By.xpath("//*[@id=\"u_0_w\"]")).click();
 
-        String parentWindow = driver.getWindowHandle();
-        System.out.println(parentWindow);
 
-        //driver.switchTo().window(parentWindow);
-
+        //9. VERIFY THAT ACCOUNT IS CREATED SUCCESSFULLY.
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"confirm_center\"]/div/div[1]/div/div[2]/h2")));
+        if (driver.findElement(By.xpath("//*[@id=\"confirm_center\"]/div/div[1]/div/div[2]/h2")).isDisplayed()){
+            System.out.println("Account is Created");
+        }else{
+            System.out.println("Account is NOT Created");
+        }
 
 
 
